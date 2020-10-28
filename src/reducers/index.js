e863ba6b402eea7e1fea80234241791de3b65656
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-10-28 15:38:38 -0500

    feat: set default store with reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import { authedUsers } from "./authedUsers";
+import { questions } from "./questions";
+import { users } from "./users";
+
+export default combineReducers({ users, questions, authedUsers });
