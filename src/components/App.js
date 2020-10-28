884200be8b2330080cf09498c95487141548cae2
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-10-28 16:22:46 -0500

    feat: clean up and added extra avatars

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 
-import "./App.css";
-
 function App() {
-  return <div>App is running</div>;
+  return (
+    <div>
+      App is running
+      <div>
+        Icons made by{" "}
+        <a href="https://www.flaticon.com/authors/dighital" title="Dighital">
+          Dighital
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          {" "}
+          www.flaticon.com
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
