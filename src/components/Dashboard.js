c71c0d363f8434d6106aaeb407008b560c6e7873
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-10-29 03:06:20 -0500

    feat: added and experimented with Material-UI

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,22 +3,22 @@ import { connect } from "react-redux";
 
 import Question from "./Question";
 
+import Grid from "@material-ui/core/Grid";
+
 class Dashboard extends Component {
   render() {
     console.log(this.props);
     return (
-      <div>
-        <h3 align="center">Home</h3>
-        <ul align="center">
+      <Grid container>
+        <h3>Home</h3>
+        <Grid container spacing={3} justify="center">
           {this.props.questionIDs.map((id) => (
-            <li key={id}>
-              <div>
-                <Question id={id} />
-              </div>
-            </li>
+            <Grid key={id} item xs={3}>
+              <Question id={id} />
+            </Grid>
           ))}
-        </ul>
-      </div>
+        </Grid>
+      </Grid>
     );
   }
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -2,10 +2,21 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
 class Question extends Component {
   render() {
     console.log(this.props);
-    return <div>Question</div>;
+    return (
+      <Card>
+        <CardHeader title="Test"></CardHeader>
+      </Card>
+    );
   }
 }
 
